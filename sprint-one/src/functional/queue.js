f66d56c3f65819099c9a427271d973a92054edcc
Manjula Manickababu <Manjula Manickababu>
2016-09-14 12:30:43 -0700

    stack complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,20 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  var counter = 0;
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    counter++;
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
+    return 0;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,24 @@
 var Stack = function() {
   var someInstance = {};
-
+  var counter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function(value) { 
+    someInstance[counter] = value;   
+    counter++;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function() {    
+    if(counter > 0){
+      counter--;
+    }
+    return someInstance[counter];
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
