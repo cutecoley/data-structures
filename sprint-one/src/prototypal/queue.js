9dc31e288ec352225b8e1b9e9dedde13679c4076
cutecoley <manjula.manicka@gmail.com>
2016-09-15 04:30:53 -0700

    sprint 1 complete

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(queueMethods)
+  someInstance.counter = 0;
+  someInstance.storage = {};
+  someInstance.incrementer = 0; 
+  
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
 
+queueMethods.enqueue = function(value) {
+this.storage[this.counter] = value;
+this.counter++;
+};
+
+queueMethods.dequeue = function() {
+if(this.incrementer < this.counter){
+  var deleteObj = this.storage[this.incrementer];
+  this.incrementer++;
+  return  deleteObj;
+}
+
+};
+
+queueMethods.size = function() {
+return this.counter - this.incrementer;
+};
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  
+  this.counter = 0;
+  this.storage = {};
+  this.incrementer = 0; 
+  
 };
 
+Queue.prototype.enqueue = function(value) {
+	this.storage[this.counter] = value;
+	this.counter++;
+};
 
+Queue.prototype.dequeue = function() {
+	if(this.incrementer < this.counter){
+	  var deleteObj = this.storage[this.incrementer];
+	  this.incrementer++;
+	  return  deleteObj;
+	}
+
+};
+
+Queue.prototype.size = function() {
+	return this.counter - this.incrementer;
+};
